967065a98cfe3d6892f03b9ff3e79bdb2172140c
HeitorSrDev <heitor_rachadel@estudante.sesisenai.org.br>
2025-05-22 09:55:22 -0300

    Corrigindo erros 1

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const fs = require('fs');
-const express = require('express')
+const express = require('express');
+const { error } = require('console');
 const app = express()
 const porta = 8000
 
+async function readData() {
+    try{
+        const data = await fs.promises.readFile('logs.txt', 'utf8');
+    return data;
+} catch (err) {
+    return{error:err.mensage}
+}
+}
+
+async function writeData(data) {
+    try{
+        await fs.appendFile('logs.txt', data);
+    } catch (err) {
+        return{error:err.mensage}
+    }
+}
+
 
 function adicionarArquivo(Name){
-    const logs = "logs.txt";
     const data = new Date.parse();
     const idUnico = data.getTime();
     let nome = new Name(nome);
@@ -25,20 +42,49 @@ class Name{
     }
 }
 
-fs.writeFileSync('logs.txt', adicionarArquivo);
-app.get('/logs', (req, res) => {
-    res.send(adicionarArquivo);
+fs.writeFileSync('logs.txt', '', (err) => {
+    if (err) {
+        console.error("Erro ao criar o arquivo:", err);
+    } else {
+        console.log("Arquivo criado com sucesso!");
+    }
 });
+
 app.listen(porta, () => {
     console.log(`Servidor rodando na porta ${porta}`);
 });
+app.use(express.json());
 
-app.get('/logs/:id', (req, res) => {
-    const logs = fs.readFileSync('logs.txt', 'utf8');
-    res.send(idUnico);
-    if (err) {
-        console.error("Erro ao ler o arquivo:", err);
-    }else{
-        console.log("Id encontrado:", idUnico);
+app.get('/logs', async (req, res) => {
+    const logs = await readData();
+    if (logs.error) {
+        res.status(500).json({ error: logs.error });
+    } else {
+        res.json(logs);
+    }
+});
+app.post('/logs', async (req, res) => {
+    const { nome } = req.body;
+    const logs = await writeData(nome);
+    if (logs.error) {
+        res.status(500).json({ error: logs.error });
+    } else {
+        res.json(logs);
+    }
+});
+
+app.get('/logs/:id', async (req, res) => {
+    const id = req.params.id;
+    const logs = await readData();
+    if (logs.error) {
+        res.status(500).json({ error: logs.error });
+    } else {
+        const log = logs.find(log => log.id === id);
+        if (log) {
+            res.json(log);
+            console.log('ID encontrado:', id);
+        } else {
+            res.status(404).json({ error: 'Id não encontrado' });
+        }
     }
 });
